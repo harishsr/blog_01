e549facf26eab20be4cd05b6e048959ed261245f
Harish Ramachandran <harishsr@icloud.com>
2016-12-16 21:41:05 -0500

    validions w/ tests for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, 
+                    length: 5..250
+  validates :content, presence: true,
+                    length: 5..40_000
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'title must exist and be in range' do
+    article = Article.new(content: 'a'*10)
+    [0, 4, 251].each do |n|
+      article.title = 'a'*n
+      refute article.valid?, "Title length #{n} should be invalid"
+    end
+
+    [5, 250].each do |n|
+      article.title = 'a'*5
+      assert article.valid?, "Title length #{n} should be valid"
+    end
+  end
+
+  test 'content must exist and be in range' do
+    article = Article.new(title: 'a'*5)
+    [0, 4, 40_001].each do |n|
+      article.content = 'a'*n
+      refute article.valid?, "Content length #{n} should be invalid"
+    end
+
+    [5, 40_000].each do |n|
+      article.content = 'a'*5
+      assert article.valid?, "Content length #{n} should be valid"
+    end
+  end
 end
